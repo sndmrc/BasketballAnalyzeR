e3006f7e530cba872e8a17503364b4fee3eb1d91
sndmrc <sandri.marco@gmail.com>
2018-12-17 20:19:36 +0100

    Added 'Tadd' dataset

---
+++ R/Tadd.R
@@ -0,0 +1,14 @@
+#' Tadd dataset - NBA 2017-2018
+#'
+#' @description In this data frame cases (rows) are teams and variables (columns) are referred to achievements of the opponents in the different games
+#'
+#' @format A data frame with 30 rows and 23 variables:
+#' \describe{
+#'   \item{Team}{Teams}
+#'   \item{Conference}{Conference}
+#'   \item{Division}{Division}
+#'   \item{Rank}{Rank}
+#'   \item{Playoff}{Playoff}
+#' }
+#' @source \url{https://github.com/sndmrc/BasketballAnalyzeR}
+"Tadd"
