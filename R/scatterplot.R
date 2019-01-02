8d10b24f9d6920a0f5904c9b0d77a8ae31d6c76b
sndmrc <sandri.marco@gmail.com>
2019-01-02 22:56:23 +0100

    Updated 'scatterplot' function

--- R/scatterplot.R
+++ R/scatterplot.R
@@ -1,8 +1,8 @@
 #' Scatter plot and matrix of scatter plot
 #'
 #' @param data A dataframe
-#' @param z Stratification variable
-#' @param z.name Name of stratification variable
+#' @param data.var A vector of characaters with name of variables used on the axes of scatter plot(s)
+#' @param z.var The stratification variable (color)
 #' @param palette Color palette
 #' @param labels Text labels for (x, y) points; only for single scatter plot
 #' @param subset Subset of points to be evidenced; only for single scatter plot
@@ -11,48 +11,71 @@
 #' @param col.subset Color for the subset of points; only for single scatter plot
 #' @param zoom X and Y axis range; only for single scatter plot
 #' @param title Plot title
+#' @param legend Set legend='no' to remove legend
 #' @param upper  See help for GGally::ggpairs
 #' @param lower  See help for GGally::ggpairs
 #' @param diag  See help for GGally::ggpairs
 #' @return A ggplot2 plot
 #' @examples
 #' # Single scatter plot
-#' data <- Pbox[, c("PTS","P3M")]
-#' scatterplot(data, labels=as.character(Pbox$Player), subset=1:3)
+#' Pbox.sel <- subset(Pbox, MIN>= 500)
+#' X <- data.frame(AST=Pbox.sel$AST/Pbox.sel$MIN,TOV=Pbox.sel$TOV/Pbox.sel$MIN)
+#' X$PTSpm <- Pbox.sel$PTS/Pbox.sel$MIN
+#' mypal <- colorRampPalette(c("blue","yellow","red"))
+#' scatterplot(X, data.var=c("AST","TOV"), z.var="PTSpm", labels=1:nrow(X), palette=mypal)
 #' # Matrix of scatter plots
-#' data <- Pbox[, c("PTS","P3M","P2M","OREB")]
-#' scatterplot(data, z=Pbox$Team)
+#' data <- Pbox[1:50, c("PTS","P3M","P2M","OREB","Team")]
+#' scatterplot(data, data.var=1:4, z.var="Team")
 #' @export
 #' @importFrom GGally ggpairs
 
-scatterplot <- function(data, z=NULL, z.name=NULL, palette=NULL, labels=NULL, repel_labels=FALSE, text_label=TRUE,
-                        subset = NULL, col.subset='gray50', zoom = NULL, title = NULL,
+scatterplot <- function(data, data.var, z.var=NULL, palette=NULL, labels=NULL, repel_labels=FALSE, text_label=TRUE,
+                        subset = NULL, col.subset='gray50', zoom = NULL, title = NULL, legend='yes',
                         upper = list(continuous = "cor", combo = "box_no_facet", discrete = "facetbar", na = "na"),
                         lower=list(continuous = "points", combo = "facethist", discrete = "facetbar", na = "na"),
                         diag = list(continuous = "densityDiag", discrete = "barDiag", na = "naDiag")) {
 
-  x <- y <- NULL
+  x <- y <- z <- NULL
   if (!is.data.frame(data)) {
-    stop("'data' must be a matrix or a data frame")
+    stop("'data' must be a data frame")
   } else if (is.data.frame(data) & ncol(data)==1) {
     stop("The number of columns in 'data' must be 2 or more")
   }
-  if (is.null(labels)) {
-    labels <- 1:nrow(data)
-  }
-  nm.vars <- names(data)
 
-  if (ncol(data)==2) {
-    if (is.null(z)) {
-      names(data) <- c("x","y")
-      p <- ggplot(data=data, aes(x=x, y=y))
+  if (is.numeric(data.var)) {
+    nm.data.vars <- names(data)[data.var]
+  } else if (is.character(data.var)) {
+    nm.data.vars <- data.var
+  } else {
+    stop("'data.var' must be numeric or character")
+  }
+
+  if (length(z.var)>1) {
+    stop("The length of 'z.var' must be 1")
+  }
+  if (!is.null(z.var)) {
+    if (is.numeric(z.var)) {
+      nm.z.var <- names(data)[z.var]
+    } else if (is.character(z.var)) {
+      nm.z.var <- z.var
     } else {
-      if (!is.numeric(z)) {
-        z <- factor(z)
+      stop("'z.var' must be numeric or character")
+    }
+  }
+
+  if (length(data.var)==2) {
+    df <- data[, data.var]
+    names(df) <- c("x","y")
+    if (is.null(z.var)) {
+      p <- ggplot(data=df, aes(x=x, y=y))
+    } else {
+      z <- data[, z.var]
+      if (is.character(z)) {
+        df$z <- factor(z)
+      } else if (is.factor(z) | is.numeric(z)) {
+        df$z <- z
       }
-      data <- cbind(data, z)
-      names(data) <- c("x","y","z")
-      p <- ggplot(data=data, aes(x=x, y=y, color=z))
+      p <- ggplot(data=df, aes(x=x, y=y, color=z))
     }
     if (!is.null(zoom)) {
       xmin <- zoom[1]
@@ -61,15 +84,19 @@ scatterplot <- function(data, z=NULL, z.name=NULL, palette=NULL, labels=NULL, re
       ymax <- zoom[4]
       p <- p + xlim(c(xmin, xmax)) + ylim(c(ymin, ymax))
     }
-    if (is.null(subset)) {
-      if (repel_labels) {
-        p <- p + ggrepel::geom_text_repel(aes(label = labels), size = 3)
+    if (is.null(subset)) { ### if 'subset' is not defined
+      if (is.null(labels)) {
+        p <- p + geom_point()
       } else {
-        p <- p + geom_text(aes(label = labels), size = 3)
+        if (repel_labels) {
+          p <- p + ggrepel::geom_text_repel(aes(label = labels), size = 3)
+        } else {
+          p <- p + geom_text(aes(label = labels), size = 3)
+        }
       }
-    } else {
-      subset1 <- data[-subset, ]
-      subset2 <- data[subset, ]
+    } else {  ### if 'subset' is defined
+      subset1 <- df[-subset, ]
+      subset2 <- df[subset, ]
       subset1.labels <- labels[-subset]
       subset2.labels <- labels[subset]
       if (is.null(labels)) {
@@ -87,30 +114,32 @@ scatterplot <- function(data, z=NULL, z.name=NULL, palette=NULL, labels=NULL, re
                                              size = 4, col = col.subset, fontface = 2)
         } else {
           p <- p + ggrepel::geom_text_repel(data = subset2, aes(label = subset2.labels),
-                                              size = 4, col = col.subset, fontface = 2)
+                                            size = 4, col = col.subset, fontface = 2)
         }
       }
     }
     if (!is.null(palette) & is.factor(z)) {
-      p <- p + scale_color_manual(palette=palette)
+      p <- p + scale_color_manual(name=nm.z.var, values=palette(length(unique(z))))
     } else if (!is.null(palette) & !is.factor(z)) {
-      p <- p + scale_color_gradientn(colors=palette(length(unique(z))))
+      p <- p + scale_color_gradientn(name=nm.z.var, colors=palette(length(unique(z))))
     }
-    p <- p + labs(title=title, x=nm.vars[1], y=nm.vars[2]) +
-      ggplot2::guides(color=guide_legend(title=z.name)) +
+    p <- p + labs(title=title, x=nm.data.vars[1], y=nm.data.vars[2]) +
       theme_bw()
-  }
-  else if (ncol(data)>2) {
-    if (is.null(z)) {
+
+  }  else if (length(data.var)>2) { ### Matrix of scatter plots
+    if (is.null(z.var)) {
+      df <- data[, data.var]
       p <- GGally::ggpairs(data, title=title,
                            lower=lower, upper=upper, diag=diag)
     } else {
+      df <- data[, data.var]
+      z <- data[, z.var]
       p <- GGally::ggpairs(data, mapping=aes(color=z), title=title,
                            lower=lower, upper=upper, diag=diag)
     }
   }
   p <- p + theme_bw()
-  if (!is.null(palette) & is.factor(z)) {
+  if (!is.null(z.var) & legend=="no") {
     p <- p + theme(legend.position="none")
   }
   print(p)
