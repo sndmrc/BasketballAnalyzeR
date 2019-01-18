9e6e677b3f6dfabf727797bb302d1d3bc2d5526a
sndmrc <sandri.marco@gmail.com>
2019-01-18 01:16:14 +0100

    Updated 'plot.corranalysis' function

--- R/plot.corranalysis.R
+++ R/plot.corranalysis.R
@@ -79,7 +79,7 @@ plot.corranalysis <- function(x, horizontal = TRUE, title = NULL, ...) {
     datanet <- ggnetwork::ggnetwork(net, layout = "circle")
     p2 <- ggplot(datanet, aes(x = x, y = y, xend = xend, yend = yend)) +
       ggnetwork::geom_nodes(shape=21, fill="#D6EAF877", color="#3498DB77", size=15) +
-      ggnetwork::geom_edges(aes(color=edge.color, alpha=edge.color), size=1.5) +
+      ggnetwork::geom_edges(aes(color=edge.color), size=1.5) +
       scale_colour_gradientn("", colors = cols, limits=c(-1,1)) +
       scale_alpha(guide=FALSE) +
       ggnetwork::geom_nodetext_repel(aes(label=vertex.names)) +
