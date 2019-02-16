3957ebed16feaa4aa01c26f7653538c614827b87
sndmrc <sandri.marco@gmail.com>
2019-02-16 01:30:16 +0100

    Updated 'networkplot' function

--- R/networkplot.R
+++ R/networkplot.R
@@ -7,10 +7,13 @@
 #' @param layout.par Paramater for network vertex layout algorithms
 #' @param edge.thr Threshold for edge values; values below the threshold are set to 0
 #' @param edge.col.lim A numeric vector of length two providing limits of the scale for edge color
+#' @param edge.col.lab Label for edge color legend
 #' @param node.data Data with variables for node size and node color
 #' @param node.size A string with the variable name for node size
+#' @param node.size.lab Label for node size legend
 #' @param node.col A string with the variable name for node color
 #' @param node.col.lim A numeric vector of length two providing limits of the scale for node color
+#' @param node.col.lab Label for node color legend
 #' @param node.pal Palette for node colors
 #' @param edge.pal Palette for edge colors
 #' @return A ggplot2 object
@@ -34,8 +37,9 @@
 
 networkplot <- function(data, assist="assist", player="player",
                         layout="kamadakawai", layout.par=list(),
-                        edge.thr=0, edge.col.lim=NULL,
-                        node.data=NULL, node.size=NULL, node.col=NULL, node.col.lim=NULL,
+                        edge.thr=0, edge.col.lim=NULL, edge.col.lab=NULL,
+                        node.data=NULL, node.size=NULL, node.size.lab=NULL,
+                        node.col=NULL, node.col.lim=NULL, node.col.lab=NULL,
                         node.pal=colorRampPalette(c("white","blue", "red")),
                         edge.pal=colorRampPalette(c("white","blue", "red"))) {
 
@@ -54,8 +58,12 @@ networkplot <- function(data, assist="assist", player="player",
                           ignore.eval=FALSE,  names.eval="N")
 
   if (is.null(node.size) & is.null(node.col)) {
-    node.col <- "% P2+P3 with\nan assist"
-    node.size <-"Pts created by\nplayer assist"
+    if (is.null(node.col.lab)) {
+      node.col.lab <- "Node color:\n% assisted shots\nmade by the player"
+    }
+    if (is.null(node.size.lab)) {
+      node.size.lab <- "Node size:\nPTS assisted by\nthe player"
+    }
     nodes1 <- data %>%
       dplyr::group_by(player) %>%
       dplyr::summarise(N2P3P_made=sum(event_type=="shot"),
@@ -71,6 +79,12 @@ networkplot <- function(data, assist="assist", player="player",
       tidyr::replace_na(list(node.col=0, node.size=0))
 
   } else if (!is.null(node.size) & !is.null(node.col)) {
+    if (is.null(node.col.lab)) {
+      node.col.lab <- paste0("Node color:\n",node.col)
+    }
+    if (is.null(node.size.lab)) {
+      node.size.lab <- paste0("Node size:\n",node.size)
+    }
     nodes <- node.data %>%
       dplyr::select(player, node.size, node.col) %>%
       dplyr::rename(node.size=!!node.size, node.col=!!node.col)
@@ -92,15 +106,18 @@ networkplot <- function(data, assist="assist", player="player",
   if (is.null(edge.col.lim)) {
     edge.col.lim <- range(tbl)
   }
+  if (is.null(edge.col.lab)) {
+    edge.col.lab <- "Edge color:\nnumber of assists"
+  }
 
   datanet <- ggnetwork::ggnetwork(net, layout=layout, layout.par=layout.par)
   p <- ggplot(datanet, aes(x = x, y = y, xend = xend, yend = yend)) +
-    ggnetwork::geom_nodes(aes(size=node.size, fill=node.col), shape=21, color="blue") +
+    ggnetwork::geom_nodes(aes(size=node.size, fill=node.col), shape=21, color="gray") +
     ggnetwork::geom_edges(aes(color=N, alpha=N), size=1.5, curvature=0.1, arrow=arrow(length=unit(6, "pt"), type="closed")) +
     ggnetwork::geom_nodetext_repel(aes(label=vertex.names)) +
-    scale_size_continuous(paste0("Node size:\n",node.size), breaks=pretty(nodes$node.size,n=7)) +
-    scale_fill_gradientn(paste0("Node color:\n",node.col), limits=node.col.lim, colors=node.pal(100)) +
-    scale_colour_gradientn("Edge color:\nAssist num.", limits=edge.col.lim, colors=edge.pal(100)) +
+    scale_size_continuous(node.size.lab, breaks=pretty(nodes$node.size,n=7)) +
+    scale_fill_gradientn(node.col.lab, limits=node.col.lim, colors=node.pal(100)) +
+    scale_colour_gradientn(edge.col.lab, limits=edge.col.lim, colors=edge.pal(100)) +
     scale_alpha(guide=FALSE) +
     ggnetwork::theme_blank() +
     guides(fill=guide_colorbar(order=1), size=guide_legend(order=3))
