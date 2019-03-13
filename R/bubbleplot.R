35cdb5418bf6a5fea1af56846f5b066545dde37b
sndmrc <sandri.marco@gmail.com>
2019-03-13 02:40:18 +0100

    Updated 'bubbleplot' function

--- R/bubbleplot.R
+++ R/bubbleplot.R
@@ -6,30 +6,48 @@
 #' @param y Name of variable on the y axis
 #' @param col Name of variable on the color axis
 #' @param size Name of variable on the size axis
+#' @param text.col Name of variable for text colors
+#' @param text.size Text font size
 #' @param scale.size If TRUE, 'size' variable is rescaled between 0 and 100.
-#' @param labels Vector of labels for 4 variables
+#' @param labels Vector of variable labels (on legend and axis)
 #' @param mx mx
 #' @param my my
 #' @param mcol mcol
 #' @param title Title for the plot
 #' @param repel Activate text repelling
+#' @param text.legend If TRUE, show the legend for text color
 #' @return A ggplot2 object
+#' @examples
+#' X <- data.frame(T=Tbox$Team, P2p=Tbox$P2p, P3p=Tbox$P3p,
+#'        FTp=Tbox$FTp, AS=Tbox$P2A + Tbox$P3A + Tbox$FTA)
+#' labs <- c("2-point shots (% made)","3-point shots (% made)",
+#'           "free throws (% made)","Total shots attempted")
+#' bubbleplot(X, id="T", x="P2p", y="P3p", col="FTp",
+#'            size="AS", labels=labs)
 #' @export
 #' @importFrom ggplot2 scale_fill_gradient2
 #' @importFrom ggplot2 scale_size_area
 #' @importFrom ggplot2 guide_legend
 #' @importFrom ggplot2 geom_hline
 #' @importFrom ggplot2 geom_vline
+#' @importFrom dplyr mutate
 
-bubbleplot <- function(data, id, x, y, col, size, scale.size=TRUE, labels = NULL, mx = NULL, my = NULL,
-                       mcol = NULL, title = NULL, repel = TRUE) {
+bubbleplot <- function(data, id, x, y, col, size, text.col=NULL, text.size=2.5, scale.size=TRUE, labels = NULL, mx = NULL, my = NULL,
+                       mcol = NULL, title = NULL, repel = TRUE, text.legend=TRUE) {
 
-  ID <- NULL
-  dts <- data %>%
-         dplyr::select(id, x, y, col, size) %>%
-         dplyr::rename(ID = !!id, x = !!x, y = !!y, col = !!col, size = !!size) %>%
-         stats::na.omit()
-
+  ID <- textColor <- NULL
+  if (is.null(text.col)) {
+    dts <- data %>%
+      dplyr::mutate(ID  = !!rlang::sym(id) , x = !!rlang::sym(x), y = !!rlang::sym(y),
+             col = !!rlang::sym(col), size = !!rlang::sym(size)) %>%
+      dplyr::select(ID, x, y, col, size)
+  } else {
+    dts <- data %>%
+      dplyr::mutate(ID  = !!rlang::sym(id) , x = !!rlang::sym(x), y = !!rlang::sym(y),
+             col = !!rlang::sym(col), size = !!rlang::sym(size),
+             textColor=!!rlang::sym(text.col)) %>%
+      dplyr::select(ID, x, y, col, size, textColor)
+  }
   if (is.null(labels)) {
     labels <- names(dts)[-1]
   }
@@ -59,11 +77,18 @@ bubbleplot <- function(data, id, x, y, col, size, scale.size=TRUE, labels = NULL
     p <- p + scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = mcol)
   }
 
-  if (repel) {
-    p <- p + ggrepel::geom_text_repel(size = 2.5)
+  if (is.null(text.col)) {
+    if (repel) {
+      p <- p + ggrepel::geom_text_repel(size = text.size)
+    } else {
+      p <- p + geom_text(size = text.size)
+    }
   } else {
-    p <- p + geom_text(size = 2.5)
+    if (repel) {
+      p <- p + ggrepel::geom_text_repel(aes(color=textColor), size = text.size, show.legend=text.legend)
+    } else {
+      p <- p + geom_text(aes(color=textColor), size = text.size, show.legend=text.legend)
+    }
   }
-
   return(p)
 }
