5384c8af4d532b81362aa41493a9dcee56ba92b1
sndmrc <sandri.marco@gmail.com>
2019-11-15 16:30:39 +0100

    Updated .onAttach message

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-    '\nIf you want to reproduce the figures contained in the book of\nZuccolotto and Manisera (2020) and\nif the version of your R machine is < 3.6.0, you need to type\nRNGkind(sample.kind = "Rounding")\nat the beginning of your working session')
+    '\nIf you want to reproduce the figures contained in the book of\nZuccolotto and Manisera (2020) and\nif the version of your R machine is >= 3.6.0, you need to type\nRNGkind(sample.kind = "Rounding")\nat the beginning of your working session')
 }
