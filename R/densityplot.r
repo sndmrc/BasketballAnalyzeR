41ff435200f4637801b3fe35d0c299b94dbafe3d
sndmrc <sandri.marco@gmail.com>
2020-03-18 16:20:50 +0100

    Update for density plot help file

--- R/densityplot.r
+++ R/densityplot.r
@@ -1,14 +1,23 @@
 #' Computes and plots kernel density estimation of shots with respect to a concurrent variable
 #'
 #' @author Marco Sandri, Paola Zuccolotto, Marica Manisera (\email{basketballanalyzer.help@unibs.it})
-#' @param data A play-by-play data frame with at least the following columns: \code{"ShotType"}, \code{"shot_distance"}, \code{"player"}, \code{"points"} and the column specified in \code{var}.
-#' @param var A character string giving the numerical variable according to which the shot density is estimated. Available options: "playlength", "periodTime", "totalTime", "shot_distance".
+#' @param data A data frame whose rows are shots and with the following columns: \code{ShotType}, \code{player}, \code{points} and at least one of \code{playlength}, \code{periodTime}, \code{totalTime}, \code{shot_distance} (the column specified in \code{var}, see Details).
+#' @param var A character string giving the numerical variable according to which the shot density is estimated. Available options: \code{"playlength"}, \code{"periodTime"}, \code{"totalTime"}, \code{"shot_distance"}.
 #' @param shot.type A character string giving the type of shots to be analyzed. Available options: \code{"2P"}, \code{"3P"}, \code{"FT"}, \code{"field"}.
 #' @param thresholds A numerical vector with two thresholds defining the range boundaries of the three regions.
 #' @param best.scorer If TRUE, displays the player who scored the highest number of points in the corresponding interval.
 #' @param period.length the length of a quarter in minutes (default: 12 minutes as in NBA).
 #' @param bw A numerical value for the smoothing bandwidth of the kernel density estimator or a character string giving a rule to choose the bandwidth (see \link[stats]{density}).
 #' @param title Plot title.
+#' @details The \code{data} dataframe could also be a play-by-play dataset provided that rows corresponding to events different from shots have \code{NA} in the \code{ShotType} variable.
+#' @details Required columns:
+#' @details *\code{ShotType}, a factor with the following levels: \code{"2P"}, \code{"3P"}, \code{"FT"} (and \code{NA} for events different from shots)
+#' @details *\code{player}, a factor with the name of the player who made the shot
+#' @details *\code{points}, a numeric variable (integer) with the points scored by made shots and \code{0} for missed shots
+#' @details *\code{playlength}, a numeric variable with time between the shot and the immediately preceding event
+#' @details *\code{periodTime}, a numeric variable with seconds played in the quarter when the shot is attempted
+#' @details *\code{totalTime}, a numeric variable with seconds played in the whole match when the shot is attempted
+#' @details *\code{shot_distance}, a numeric variable with the distance of the shooting player from the basket (in feet)
 #' @references P. Zuccolotto and M. Manisera (2020) Basketball Data Science: With Applications in R. CRC Press.
 #' @return A \code{ggplot2} plot
 #' @examples
